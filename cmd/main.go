a629daa6485a25d548aa84fb870278fd4e301e7b
Alexander Chirkov <chirkov.a.y@yandex.ru>
2025-08-15 12:16:06 +0300

    Add main

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"L0-wb/internal/kafkaconsumer"
+	"L0-wb/internal/repository/postgres"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
 
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
 	fmt.Println("start")
 
+	// Подключение к базе данных
+	dsn := os.Getenv("DATABASE_DSN")
+	pool := postgres.Connect(dsn)
+	err := pool.Ping(context.Background())
+	if err != nil {
+		fmt.Printf("err conection db: %s", err)
+		return
+	} else {
+		fmt.Println("Sucses connetion db")
+	}
+
+	// Создание репозитория
+	repo := postgres.NewRepository(pool)
+
+	// Создание консьюмера
+	consumer := kafkaconsumer.NewConsumer(
+		[]string{os.Getenv("KAFKA_BROKER")},
+		os.Getenv("KAFKA_TOPIC"),
+		os.Getenv("KAFKA_GROUP_ID"),
+		repo)
+
+	// Запуск консьюмера
+	go func() {
+		log.Println("Kafka consumer started")
+		if err := consumer.Run(ctx); err != nil {
+			log.Fatalf("Kafka consumer stopped: %v", err)
+			cancel()
+		}
+	}()
+
+	// // Запуск http сервера
+	// go func() {
+	// 	log.Println("HTTP server started")
+	// 	if err := http.ListenAndServe(addr, httpSrv); err != nil && err != http.ErrServerClosed {
+	// 		log.Printf("HTTP server stopped: %v", err)
+	// 		cancel()
+	// 	}
+	// }()
+
+	// Ожидание сигнала завершения
+	<-ctx.Done()
+	log.Println("Shutting down...")
+
+	time.Sleep(time.Second)
 }
